ce3b29dccd96524c70cd8198dc7b996056495501
melaniehoff <hoffmelanie@gmail.com>
2016-06-23 19:36:27 -0400

    “commit”

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/env bash
-PROJECT_PATH=$( cd $(dirname $0) ; pwd -P )
-CONFIG_FILE=$PROJECT_PATH/_config.yml
-if [ -f $CONFIG_FILE ]; then rm $CONFIG_FILE; fi
-cp _prodconfig.yml $CONFIG_FILE
 hexo clean
 hexo generate
 hexo deploy
+git add -A 
+git commit -m “commit”
+git push origin master:save
